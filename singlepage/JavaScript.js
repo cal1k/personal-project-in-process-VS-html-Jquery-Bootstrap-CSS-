56224d55183647aea2efcd79928a741ed8bf4c4f
cal1k <calvin.taylor@live.com>
2017-10-29 14:56:06 -0500

    Initial commit

---
+++ singlepage/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function ()) {
+        $('#appear').each(function (i) {
+            var bottom = $(this).offset().top + $(this).outerHeight();
+            var bottom = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+
+            }
+        });
+    });
+});
